58fb38d520214705b2250d23545d2761825f9edf
Isaacoun100 <isaac@Isaac-Linux>
2020-03-03 17:58:02 -0600

    [Main call] The main method now calls the MainJFrame (User Interface)

--- src/com/UserInterface/MainMessenger.java
+++ src/com/UserInterface/MainMessenger.java
@@ -1,7 +1,5 @@
 package com.UserInterface;
 
-import com.Portip.VerifyPort;
-
 /**
  *
  * @author Isaac Herrera Monge
@@ -9,11 +7,9 @@ import com.Portip.VerifyPort;
 public class MainMessenger {
     
     public static void main(String[] args) {
-        VerifyPort conectPort = new VerifyPort();
-        conectPort.setIP("127.0.0.1");
-    
-        System.out.println(conectPort.getPort(args));
-    
+        
+        MainJFrame MainWindow = new MainJFrame();
+        MainWindow.setVisible(true);
         
     }
     
