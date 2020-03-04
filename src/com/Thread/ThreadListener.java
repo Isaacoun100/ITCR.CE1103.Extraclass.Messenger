2028be506f740695f4c03c16f2523ddf6936f071
Isaacoun100 <isaac@Isaac-Linux>
2020-03-04 12:51:18 -0600

    [Javadoc comments created] Javadoc comments added

--- src/com/Thread/ThreadListener.java
+++ src/com/Thread/ThreadListener.java
@@ -5,45 +5,44 @@
  */
 package com.Thread;
 
-import com.Comunication.Receiver;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import com.Comunication.Transmitter;
 /**
- *
- * @author isaac
+ *ThreadListener waits for a response from
+ * either the server or the client to give an
+ * output(message)
+ * 
+ * @author Isaac Herrera Monge
  */
 public class ThreadListener extends Thread {
-    
-    BufferedReader entrada;
-    
-    Receiver Listen = new Receiver();
+Transmitter SendClient;   
 
-        String message;
-   
-    public ThreadListener(String message){
-     
-        this.message = message;
+int Port;
+
+    /**
+     * ThreadListener() initializes the variables
+     * to get them ready once the Thread is running
+     * and reduce errors
+     * 
+     * @param Port is the port in which the connection is going to be held
+     * @param IP is the IP in which the connection is going to be held
+     */
+
+    public ThreadListener(int Port, String IP) {
+        this.Port = Port;
+        this.IP = IP;
     }
-        
-    @Override
+String IP;        
+
+    /**
+     * run is a method inherited from Thread
+     * that runs whatever he has inside 
+     * on the background
+     */
+@Override
     public void run(){
         
-      while(true){
-          
-          try {
-              message = entrada.readLine();
-              System.out.println(message);
-          } catch (IOException ex) {
-              
-              System.out.println("No message");
-              
-          }
-          
-      }  
-        
+        SendClient = new Transmitter();
+        SendClient.start(IP, Port);
     }
-    
+        
 }
