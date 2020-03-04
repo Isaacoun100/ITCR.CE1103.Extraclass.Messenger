a959ac5843e5dcb1f5f32829b505619603438751
Isaacoun100 <isaac@Isaac-Linux>
2020-03-04 12:31:21 -0600

    [Javadoc comments created] Javadoc comments added

--- src/com/Portip/VerifyPort.java
+++ src/com/Portip/VerifyPort.java
@@ -8,7 +8,10 @@ import java.net.URL;
 import java.net.UnknownHostException;
 /**
  *
- * @author isaac
+ * VerifyPort checks for the available ports
+ * to start a server
+ * 
+ * @author Isaac Herrera Monge
  */
 
 public class VerifyPort {
@@ -17,9 +20,18 @@ public class VerifyPort {
     String IPaddress;
     int Port;
 
+    /**
+     * 
+     * @return the IP Address of the host if 
+     * there's no connection, throws the local host
+     * @throws IOException if network error
+     */
     
     @SuppressWarnings("UseSpecificCatch")
     public String getIP() throws IOException{
+        
+        String localhost= "127.0.0.1";
+        
         try{
             URL whatismyip = new URL("http://checkip.amazonaws.com");
             BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
@@ -30,14 +42,27 @@ public class VerifyPort {
         }
         catch(Exception e){
             
-            return "127.0.0.1";
+            return localhost;
            
         }
     }
     
+    /**
+     * 
+     * VerifyPort initializes the IPaddress variable
+     * 
+     */
+    
     public VerifyPort() {
         this.IPaddress = "127.0.0.1";
     }
+    
+    /**
+     * Assigns the IP String to the already initialized
+     * IPaddress that has a default method of local host
+     * 
+     * @param IP  refers to the IP that is going to connect to
+     */
 
     public void setIP(String IP){
         
@@ -45,6 +70,13 @@ public class VerifyPort {
         
     }
 
+    /**
+     * This method searches for the available ports, and
+     * gives the lower port available, if the port is busy then
+     * it will search for the second nearest
+     * 
+     * @return i that contains the port integer that is available
+     */
     
     public int getPort(){
         
