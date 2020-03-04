43a76cb3819a5edc74375beac1f91a1e1ea8c765
Isaacoun100 <isaac@Isaac-Linux>
2020-03-04 00:13:20 -0600

    [Verify IP Added] Now the IP get verified using amazon IP service

--- src/com/Portip/VerifyPort.java
+++ src/com/Portip/VerifyPort.java
@@ -1,19 +1,40 @@
 package com.Portip;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
 import java.net.UnknownHostException;
-
 /**
  *
  * @author isaac
  */
+
 public class VerifyPort {
 
     
     String IPaddress;
     int Port;
 
+    
+    @SuppressWarnings("UseSpecificCatch")
+    public String getIP() throws IOException{
+        try{
+            URL whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+            
+            String ip = in.readLine(); 
+            
+            return ip;
+        }
+        catch(Exception e){
+            
+            return "127.0.0.1";
+           
+        }
+    }
+    
     public VerifyPort() {
         this.IPaddress = "127.0.0.1";
     }
@@ -25,14 +46,11 @@ public class VerifyPort {
     }
 
     
-    public int getPort(String [] args){
+    public int getPort(){
         
         
       Socket socket;
-      
-      if (args.length > 0) {
-         IPaddress = args[0];
-      }
+
       
       int i;
       
